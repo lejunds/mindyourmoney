8793509f7f26f6ba78004ac81baa6726f2e2a5e1
Rishvic Pushpakaran <rishvic@gmail.com>
2025-12-20 05:07:32 +0530

    feat: Add rupee favicons and update license headers
    - Add multiple favicon formats (.ico, .png, .svg) for the rupee symbol.
    - Update +layout.svelte to use the new favicon assets for improved
      browser support.
    - Embed rupee symbol in favicon.svg.
    - Include license headers in Svelte files.
    - Add packageManager field to package.json.
    - Add attribution for rupee icons in +page.svelte.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,33 @@
+<!--
+  Mind Your Money - a personal finance management application
+  Copyright (C) 2025  Lejunds
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import faviconIco from '$lib/assets/favicon.ico';
+	import faviconPng from '$lib/assets/favicon-96x96.png';
+	import faviconSvg from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" href={faviconPng} sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href={faviconSvg} />
+	<link rel="shortcut icon" href={faviconIco} />
+</svelte:head>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<!--
+  Mind Your Money - a personal finance management application
+  Copyright (C) 2025  Lejunds
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	<a href="https://www.flaticon.com/free-icons/rupees" title="rupees icons">
+		Rupees icons created by NajmunNahar - Flaticon
+	</a>
+</p>
